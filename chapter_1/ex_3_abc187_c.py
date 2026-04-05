62dbc17c316ad8b549e22edc5657d660104d8328
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-04-05 23:01:25 +0200

    chapter 1: began work on exercice 3

---
+++ chapter_1/ex_3_abc187_c.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+
+def solve():
+    num_strings = int(input())
+    strings: set[str] = set()
+    string_counts: dict[str, int] = defaultdict(int)
+
+    for _ in range(num_strings):
+        current_string = input()
+        strings.add(current_string.lstrip("!"))
+        string_counts[current_string] += 1
+
+    for string in strings:
+        if string_counts[string] == 1:
+            if string_counts.get("!" + string, 0) == 1:
+                return string
+    return "satisfiable"
+
+
+if __name__ == "__main__":
+    print(solve())
