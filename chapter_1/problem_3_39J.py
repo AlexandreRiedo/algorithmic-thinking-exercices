8eedfbd45a5315b5a2cdc2a93f9bb16d78914991
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-04-05 19:55:45 +0200

    chapter1: problem 3 solved (finally!)

--- chapter_1/problem_3_39J.py
+++ chapter_1/problem_3_39J.py
@@ -1,15 +1,26 @@
 import sys
- 
-str1 = sys.stdin.readline()
-str2 = sys.stdin.readline()
- 
+from itertools import zip_longest
+
+str1 = sys.stdin.readline().strip()
+str2 = sys.stdin.readline().strip()
+
 num_deletable_symbols = 0
 position_deletable_symbols = []
- 
-for index in range(len(str1)):
-    if str1[0:index] + str1[index + 1 :] == str2:
-        num_deletable_symbols += 1
-        position_deletable_symbols.append(str(index + 1))
- 
+
+for index, (char1, char2) in enumerate(zip_longest(str1, str2)):
+    if char1 != char2:
+        if index < len(str1) - 1 and str1[index + 1:] != str2[index:]:
+            break
+
+        same_char_start_index = index
+        while same_char_start_index >= 0 and str1[same_char_start_index] == char1:
+            same_char_start_index -= 1
+        same_char_start_index += 1
+
+        for index in range(same_char_start_index, index + 1):
+            num_deletable_symbols += 1
+            position_deletable_symbols.append(str(index + 1))
+        break
+
 sys.stdout.write(f"{num_deletable_symbols}\n")
 sys.stdout.write(" ".join(position_deletable_symbols))
