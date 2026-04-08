53413d2643cc36209e56ee558204ccc2848fb341
Alexandre Riedo <alexandre.riedo@etu.unige.ch>
2026-04-08 02:58:47 +0200

    chapter 2: exercice 2 solved

--- chapter_2/ex_2_scode.py
+++ chapter_2/ex_2_scode.py
@@ -1,67 +1,53 @@
+import itertools
+
 with open("scode.in", "r") as file:
     final_encrypted = file.readline().strip()
 
 
-def solve(s: str) -> int:
-    if len(s) == 2:
-        return 1
+def op_1(s: str) -> str:
+    return s[1:] + s
+
+
+def op_2(s: str) -> str:
+    return s[:-1] + s
+
+
+def op_3(s: str) -> str:
+    return s + s[:-1]
+
+
+def op_4(s: str) -> str:
+    return s + s[1:]
+
+
+def solve(s: str, count) -> None:
+    if len(s) <= 2:
+        return
     else:
-        sum = 0
-        for n in range(1, len(s)):
-            if s[:n] == s[n:][-n:]:
-                sum += solve(s[n:])
-            if s[:n] == s[n:][:n]:
-                sum += solve(s[n:])
-            if s[-n:] == s[:-n][-n:]:
-                sum += solve(s[:-n])
-            if s[:n] == s[n:][:n]:
-                sum += solve(s[n:])
-        return sum
+        to_explore = set()
+        for i in range(2, len(s)):
+            prefix = s[:i]
+            to_explore.add(prefix)
+        for j in range(1, len(s) - 1):
+            suffix = s[j:]
+            to_explore.add(suffix)
+        for substring in to_explore:
+            if op_1(substring) == s:
+                next(count)
+                solve(substring, count)
+            if op_2(substring) == s:
+                next(count)
+                solve(substring, count)
+            if op_3(substring) == s:
+                next(count)
+                solve(substring, count)
+            if op_4(substring) == s:
+                next(count)
+                solve(substring, count)
+        return
 
 
+count = itertools.count()
 with open("scode.out", "w") as file:
-    file.write(str(solve(final_encrypted)))
-
-"""
-ABCD
-
-BCD + ABCD(og)
-ABC + ABCD(og)
-ABCD(og) + BCD
-ABCD(og) + ABC
-
-
-OP1
-BCDABCD -> ABCD
-Inverse
-    take first n chars away and check if the last n are the same
-    BCD + ABCD
-
-OP2
-ABCABCD -> ABCD
-idea:
-    take first n chars away and check if the first n chars are the same
-
-OP3
-ABCDBCD -> ABCD
-idea:
-    take last n chars away, check if last n chars are the same
-
-OP4
-ABCDABC -> ABCD
-idea:
-    take first n char way, check if first n chars are the same
-"""
-
-
-"""
-1. AB+ABA
-OP3 with n=2 -> ABA
-
-2. ABA+BA
-OP4 with n=2 -> ABA
-
-3. AB+ABA
-OP3 with n=2 -> AB+A
-OP2 with n=2 -> AB
-"""
+    solve(final_encrypted, count)
+    file.write(str(next(count)))
